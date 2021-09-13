2d4cd1a1c9c76787c35830eaf1f0125be7b9515a
dev-carol <contato.caroline1999@gmail.com>
2021-09-12 22:24:31 -0300

    resolves problems with npm

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { useState, useEffect } from 'react';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
@@ -9,32 +10,34 @@ type ChartData = {
 }
 const DonutChart = () => {
 
-    //FORMA ERRADA
-    let chartData : ChartData ={labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    useEffect(() => {
+        axios.get(`${BASE_URL} /sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellername);
+                const mySeries = data.map(x => x.sum);
 
-    axios.get(`${BASE_URL} /sales/amount-by-seller`)
-    .then(response =>{
-        const data = response.data as SaleSum[];
-        const myLabels = data.map( x => x.sellername);
-        const mySeries = data.map (x => x.sum);
+                setChartData = { labels: myLabels, series: mySeries };
 
-        chartData = {labels: myLabels, series: mySeries};
-            console.log(chartData); 
-    });
+            });
+
+    }, []);
+
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+
     const options = {
         legend: {
             show: true
         }
     }
-    return (  
+    return (
         <Chart
-            options={{ ...options, labels: chartData.labels}}
-            series ={chartData.series}
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
